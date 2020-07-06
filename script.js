a250d761cfba388c8d9e1af2e169042542e7ee13
samjyi95 <samjyi95@gmail.com>
2020-07-05 18:18:56 -0700

    tabs implemented

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('welcome')
+function openTab(evt, section) {
+    var i, tabz, tablinks //why do list variables here again?
+    tabz = document.getElementsByClassName('tabz');
+    for (i = 0; i < tabz.length; i++) {
+        tabz[i].style.display = "none";
+        console.log('Success')
+    }
+    tablinks = document.getElementsByClassName('tablinks')
+    for (i = 0; i < tabz.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "" );
+        console.log('Running thru fine')
+    }
+    document.getElementById(section).style.display = "block";
+    evt.currentTarget.className += " active";
+    console.log('Ran thru but did it make changes?')
+}
+document.getElementById("defaultOpen").click();
